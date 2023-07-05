e6c8e1d4689359bedc063e4ce1577a225c97e2d1
anyiamvictor <anyaimvictor@gmail.com>
2023-07-05 14:49:33 +0100

    updated my style and added logic

--- script.js
+++ script.js
@@ -1,31 +1,102 @@
 'use strict'
 
-const bookInput = document.getElementById('user-input_book').value;
+const bookTitle = document.getElementById('user-input_book_title');
+const bookAuthor = document.getElementById('user-input_author');
+const pageNumbers = document.getElementById('user-input_page_numbers');
+const toggleRead = document.querySelector('#toggle');
+const bookSummary = document.getElementById('user-input_summary');
+let bookSection = document.querySelector('.add_books_section');
+
+
 const stars = document.querySelectorAll('.star');
 const rating = document.querySelector('#rating');
-const toggleRead = document.querySelector('#toggle');
 const openModalBtn = document.getElementById('openModalBtn');
 const closeModalBtn = document.getElementById('closeModalBtn');
 const modal = document.getElementById('modal');
 const btnSubmit = document.querySelector('#btn-submit');
+const closeBook = document.getElementById('book_close');
+const newBook = document.querySelector('.new_book');
+const libraryform = document.querySelector('#library-form');
+const formControl = document.querySelector('.check-rate');
+let rate_value;
+
+
+
 
 let myLibrary = [];
 
-function Book(title, author, numberOfpages, isReadState, rating ) {
+function Book(title, author, numberOfpages, isReadState, rating , bookSummarization) {
     this.title = title;
     this.author = author;
     this.pages = numberOfpages;
     this.isRead = isReadState;
     this.rating = rating;
-
+    this.bookSummarization = bookSummarization;
 }
 
 
+
+
 // A modal window for the form
-openModalBtn.addEventListener('click', () => {
+openModalBtn.addEventListener('click', () => {   
+    libraryform.reset();
     modal.style.display = 'block';
-    document.style.background='gray'
 });
+
+
+libraryform.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let bookTitleValue = bookTitle.value;
+    let bookAuthorValue = bookAuthor.value;
+    let pageNumbersValue = pageNumbers.value;
+    let bookSummaryValue = bookSummary.value;
+    let toggleReadValue;
+    if (toggleRead.checked) toggleReadValue = 'Finished Reading';
+    if (!toggleRead.checked) toggleReadValue = 'In Progress';
+    const bookItem = new Book(
+        bookTitleValue,
+        bookAuthorValue,
+        pageNumbersValue,
+        toggleReadValue,
+        rate_value,
+        bookSummaryValue);
+  
+    console.log(bookItem);
+
+    const newBookHTML =
+      `<div class="new_book">
+      <h3 class=" new_book_h"> Book Title:</h3><p class=" new_book_p" id="book_title_content">${bookTitleValue}</p>
+      <h3 class=" new_book_h"> Book Author:</h3><p class=" new_book_p" id="book_title_content">${bookAuthorValue}</p>
+      <h3 class=" new_book_h"> Number of Pages:</h3><p class=" new_book_p" id="book_title_content">${pageNumbersValue}</p>
+      <h3 class=" new_book_h"> My Rating:</h3><p class=" new_book_p" id="book_title_content">${rate_value ? rate_value : '0'} Star(s)</p>
+      <h3 class=" new_book_h"> Status:</h3><p class=" new_book_p" id="book_title_content">${toggleReadValue}</p>
+      <h3 class=" new_book_h" id="summary_heading"> Summary</h3><p class=" new_book_p" id="book_title_summary">${bookSummaryValue?bookSummaryValue:'<i>No Summary Provided</i>'}</p>
+      <span class="book_close" id="book_close"></span>
+        </div>`;
+    bookSection.insertAdjacentHTML('beforeend', newBookHTML);
+
+const bookCloseElements = document.querySelectorAll('.book_close');
+  // Add click event listener to each book_close element
+  bookCloseElements.forEach((bookClose) => {
+    bookClose.addEventListener('click', () => {
+      // Get the parent new_book element and remove it
+      const newBook = bookClose.closest('.new_book');
+      newBook.remove();
+    });
+  });
+    
+        this.reset();
+        // reset the star rating
+        stars.forEach((star) => {
+        star.classList.remove('active');
+        });
+        rating.textContent = '0 Star Rating';
+        rate_value = 0;
+        modal.style.display = 'none';
+    
+    
+    });
+
 
 closeModalBtn.addEventListener('click', () => {
   modal.style.display = 'none';
@@ -37,10 +108,9 @@ window.addEventListener('click', (event) => {
   }
 });
 
-
 //Star Rating feauture for the books
 stars.forEach((star, index) => {
-    star.addEventListener('click', () => {
+        star.addEventListener('click', () => {
        
     if (star.classList.contains('active')) {
       // If the clicked star is already active, remove 'active' class from all stars
@@ -48,7 +118,8 @@ stars.forEach((star, index) => {
         s.classList.remove('active');
       });
       rating.textContent = '0 Star Rating';
-    } else {
+    } else
+    {
       // If the clicked star is not active, update the rating and add 'active' class to the clicked star and previous siblings
       stars.forEach((s, i) => {
         if (i <= index) {
@@ -57,8 +128,9 @@ stars.forEach((star, index) => {
           s.classList.remove('active');
         }
       });
-      const rate_value = index + 1;
-      rating.textContent = `${rate_value} Star Rating`;
+       rate_value = index + 1;
+        rating.textContent = `${rate_value} Star Rating`;
+        return rate_value;
     }
   });
 });
